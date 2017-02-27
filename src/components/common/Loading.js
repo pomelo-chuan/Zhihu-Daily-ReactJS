854a4e4e0ac7ff2d8c108e6994ed4dea616442c2
陈川 <chenchuan@appledeMacBook-Pro.local>
2017-02-27 09:52:08 +0800

    change the loading style

--- src/components/common/Loading.js
+++ src/components/common/Loading.js
@@ -3,9 +3,7 @@ import style from '../../styles/Loading.css'
 
 function Loading({ loading }) {
   return (
-    <div hidden={!loading} className={style.loading}>
-      <h1>LOADING</h1>
-    </div>
+    <div className={style.spinner} hidden={!loading}> </div>
   );
 }
 
--- src/components/common/Title.js
+++ src/components/common/Title.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import style from '../../styles/title.css'
 
 const Title = (props) => {
   return (
-    <div className={`m1 h6 pl1 bold`} style={{borderLeft: "4px solid grey", color: "grey"}}>
+    <div className={`${style.title} m1 h6 pl1 bold`} style={{borderLeft: "4px solid grey", color: "grey"}}>
       {props.title}
     </div>
   );
