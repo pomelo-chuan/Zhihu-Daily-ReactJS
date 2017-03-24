1e5f9e713b9972d375b66af979f886acb62a13f2
chenchuan <chenchuan@wosai-inc.com>
2017-03-24 10:00:01 +0800

    Fix a bug

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Router, Route, IndexRoute} from 'dva/router';
+import {Router, Route, IndexRoute, Redirect} from 'dva/router';
 import Home from './routes/Home';
 import IndexPage from './routes/IndexPage';
 import NewsDetails from './routes/NewsDetails';
@@ -8,6 +8,8 @@ import List from './routes/List'
 function RouterConfig({history}) {
   return (
     <Router history={history}>
+
+      <Redirect from="/" to="/detail" />
 
       <Route path="/detail" component={Home}>
         <IndexRoute component={IndexPage}/>
